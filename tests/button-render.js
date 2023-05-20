3fc1def3e550a9bdf37c2af6292f7f45d130e750
SERGIO LUIZ MORAL MARQUES <P226024@acd.puc-campinas.edu.br>
2023-05-19 21:47:13 -0300

    testes funcionais

---
+++ tests/button-render.js
@@ -0,0 +1,36 @@
+const chrome = require('selenium-webdriver/chrome');
+const webdriver = require('selenium-webdriver');
+const {Builder,Browser, By} = require('selenium-webdriver')
+
+const screen = { width: 1024, height: 720};
+
+(async function buttonRender(){
+
+    console.log("Iniciando");
+
+    var capabilities = webdriver.Capabilities.chrome();
+    capabilities.set('chromeOptions', {'args': ['--no-sandbox']});
+
+    let driver = await new Builder()
+        .forBrowser(Browser.CHROME)
+        .withCapabilities(capabilities)
+        .setChromeOptions( new chrome.Options().headless().windowSize(screen))
+        .build()
+
+    try{
+        await driver.get('http://localhost:3000/ht');
+
+        let btn = await driver.findElement(By.id('sendbutton'));
+        let didSendButtonRender = btn.isDisplayed()
+        if (!didSendButtonRender){
+            throw new Error('Send button not render properly');
+        }
+
+
+
+    } finally{
+        await driver.quit();
+    }
+
+} 
+)();
