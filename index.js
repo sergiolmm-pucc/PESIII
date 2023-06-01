0e8ada697999c5d194fa7fd273e39b4628e278d3
Rafael Mayer <mayer.rafa@outlook.com>
2023-05-31 22:12:22 -0300

    Grupo 1
    Pagiona de cálculo de markup

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ const INDEX = '/index.html';
 const server = express()
 
 
-server.listen(PORT, () => console.log('Escutando em ${PORT}'));
+server.listen(PORT, () => console.log(`Escutando em ${PORT}`));
 
 server.get('/', function(req,res){
     res.send("Site de tecnologia");
 });
 
 server.get('/ht',(req,res) => res.sendFile(INDEX, {root: __dirname } ) );
+
+server.get('/MKP', (req, res) => res.sendFile('./pages/markup.html', { root: __dirname } ))
