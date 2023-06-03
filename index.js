551e8d4c2acc063ecec13ffe58d0bc5e5d1c628b
pedromonteiru <80863808+pedromonteiru@users.noreply.github.com>
2023-06-03 00:42:37 +0000

    update: Adding MARKUP2

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var express = require('express');
+const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
 
 
 const server = express()
+server.use(bodyParser.json());
 
 
 server.listen(PORT, () => console.log(`Escutando em ${PORT}`));
@@ -18,3 +20,18 @@ server.get('/ht',(req,res) => res.sendFile(INDEX, {root: __dirname } ) );
 server.get('/MKP', (req, res) => res.sendFile('./pages/markup.html', { root: __dirname } ))
 
 server.get('/INFS', (req, res) => res.sendFile('./pages/INFS/.html', { root: __dirname } ))
+
+server.get('/MKP2', (req, res) => {
+    res.sendFile('./pagesMarkup2/markup2.html', { root: __dirname });
+  });
+
+server.post('/MKP2', (req, res) => {
+    const custo = req.body.custo;
+    const markup = req.body.markup;
+  
+    if (!custo || !markup) {
+      return res.status(400).json({ error: 'Custo e markup são obrigatórios' });
+    }
+    const precoVenda = custo * (1 + markup / 100);
+    res.json({ precoVenda });
+  });
