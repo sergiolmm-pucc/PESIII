23d2411170048ffe07f85f112923122acfdf6871
Rafael Mayer <mayer.rafa@outlook.com>
2023-06-07 18:39:20 -0300

    grupo 1: finalizado testes markup

--- pages/Grupo01/markup.js
+++ pages/Grupo01/markup.js
@@ -1,4 +1,5 @@
 function calculaMarkup(valorVenda, valorCompra) {
+    if(!valorVenda | !valorCompra) return 0
     return ((valorVenda - valorCompra) / 100) * 100;
 }
 
--- tests/grupo1-markup.test.js
+++ tests/grupo1-markup.test.js
@@ -1,19 +1,52 @@
+const { describe } = require('node:test')
 const calculaMarkup = require('../pages/Grupo01/markup')
 
-test('Teste função grupo 1 de calculo de Markup (1000, 100)', () => {
+// Teste função grupo 1 de calculo de Markup
+
+test('(1000, 100) = 900', () => {
     const res = calculaMarkup(1000, 100)
 
     expect(res).toBe(900)
 })
 
-test('Teste função grupo 1 de calculo de Markup (850, 100)', () => {
+test('(850, 100) = ', () => {
     const res = calculaMarkup(850, 100)
 
     expect(res).toBe(750)
 })
 
-test('Teste função grupo 1 de calculo de Markup (950, 10)', () => {
+test('(850, 10) = 840', () => {
     const res = calculaMarkup(850, 10)
 
     expect(res).toBe(840)
+})
+
+test('(850, 0) = 0', () => {
+    const res = calculaMarkup(850, 0)
+
+    expect(res).toBe(0)
+})
+
+test('(0, 0) = 0', () => {
+    const res = calculaMarkup(0, 0)
+
+    expect(res).toBe(0)
+})
+
+test('(undefined, 50) = 0', () => {
+    const res = calculaMarkup(undefined, 50)
+
+    expect(res).toBe(0)
+})
+
+test('(50, undefined) = 0', () => {
+    const res = calculaMarkup(50, undefined)
+
+    expect(res).toBe(0)
+})
+
+test('(undefined, undefined) = 0', () => {
+    const res = calculaMarkup(undefined, undefined)
+
+    expect(res).toBe(0)
 })
