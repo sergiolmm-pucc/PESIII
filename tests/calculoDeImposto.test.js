0e43436923d0956251d424dc499bd4e403b1a74a
Felipe Andreas Silva <you@example.com>
2023-06-08 14:25:51 -0300

    Modificacao do teste do grupo04

--- tests/calculoDeImposto.test.js
+++ tests/calculoDeImposto.test.js
@@ -1,16 +1,23 @@
 
 const calculoDeImposto = require('../pages/Grupo04/calculoDeImposto');
 
-test('calcularImposto calcula corretamente os impostos', () => {
+test('O Calculo de imposto estao corretos', () => {
   // Chamar a função e obter o resultado
   const resultado = calculoDeImposto(1000); // Supondo um valor de serviço de 1000
   
   // Verificar se os valores estão corretos
-  expect(resultado.cpp).toBe('280.00');
-  expect(resultado.iss).toBe('50.00');
-  expect(resultado.pis).toBe('6.50');
-  expect(resultado.csll).toBe('15.00');
-  expect(resultado.irpj).toBe('80.00');
-  expect(resultado.cofins).toBe('30.00');
-  expect(resultado.totalImpostos).toBe('461.50');
+  expect(resultado.cpp).toBe('5.88');
+  expect(resultado.iss).toBe('1.05');
+  expect(resultado.pis).toBe('0.14');
+  expect(resultado.csll).toBe('0.32');
+  expect(resultado.irpj).toBe('168');
+  expect(resultado.cofins).toBe('0.63');
+  expect(resultado.totalImpostos).toBe('9.69');
+});
+test('O total de impostos esta correto', () => {
+  // Chamar a função e obter o resultado
+  const resultado = calculoDeImposto(1000); // Supondo um valor de serviço de 1000
+  
+  // Verificar se os valor do total esta correto
+  expect(resultado.totalImpostos).toBe('9.69');
 });
