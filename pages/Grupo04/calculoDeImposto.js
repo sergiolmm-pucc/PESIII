7b17ba2648bf500cf932159817cb41864011a5e0
Felipe Andreas Silva <you@example.com>
2023-06-07 22:06:45 -0300

    test unitario do grupo04

---
+++ pages/Grupo04/calculoDeImposto.js
@@ -0,0 +1,25 @@
+function calcularImposto(valorServico) {
+    var cpp = valorServico * 0.28; 
+    var iss = valorServico * 0.05; 
+    var pis = valorServico * 0.0065; 
+    var csll = valorServico * 0.015; 
+    var irpj = valorServico * 0.08; 
+    var cofins = valorServico * 0.03; 
+    
+    var totalImpostos = cpp + iss + pis + csll + irpj + cofins;
+   
+    return {
+      cpp: cpp.toFixed(2),
+      iss: iss.toFixed(2),
+      pis: pis.toFixed(2),
+      csll: csll.toFixed(2),
+      irpj: irpj.toFixed(2),
+      cofins: cofins.toFixed(2),
+      totalImpostos: totalImpostos.toFixed(2)
+    };
+  }
+  
+  
+
+  module.exports = calcularImposto;
+  
---
+++ tests/calculoDeImposto.test.js
@@ -0,0 +1,16 @@
+
+const calculoDeImposto = require('../pages/Grupo04/calculoDeImposto');
+
+test('calcularImposto calcula corretamente os impostos', () => {
+  // Chamar a função e obter o resultado
+  const resultado = calculoDeImposto(1000); // Supondo um valor de serviço de 1000
+  
+  // Verificar se os valores estão corretos
+  expect(resultado.cpp).toBe('280.00');
+  expect(resultado.iss).toBe('50.00');
+  expect(resultado.pis).toBe('6.50');
+  expect(resultado.csll).toBe('15.00');
+  expect(resultado.irpj).toBe('80.00');
+  expect(resultado.cofins).toBe('30.00');
+  expect(resultado.totalImpostos).toBe('461.50');
+});
