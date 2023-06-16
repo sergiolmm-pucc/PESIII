ae105aba8a18d288315cd093c25af1d95b023cb4
Felipe Andreas Silva <you@example.com>
2023-06-15 21:45:05 -0300

    Correcao do teste funcional do grupo04

--- tests/funcional/grupo4-calculoINFS-func.js
+++ tests/funcional/grupo4-calculoINFS-func.js
@@ -20,8 +20,8 @@ const { Builder, By, until } = require('selenium-webdriver');
     await driver.sleep(3500);
 
     await driver.takeScreenshot().then((image, err) => {
-      require('fs').writeFile('start_grupo04.png', image, 'base64', function(err) {
-        console.log("Erro: " + err);
+      require('fs').writeFile('startgrupo04.png', image, 'base64', function(err) {
+        console.log("Erro1: " + err);
       });
     });
 
@@ -35,32 +35,31 @@ const { Builder, By, until } = require('selenium-webdriver');
     await driver.sleep(5000);
 
     await driver.takeScreenshot().then((image, err) => {
-      require('fs').writeFileSync('end_grupo04.png', image, 'base64', (err) => {
-        console.log(err);
+      require('fs').writeFileSync('endgrupo04.png', image, 'base64', (err) => {
+        console.log("Erro2: " + err);
       });
     });
 
     const resultElement = await driver.findElement(By.id('resultadoDoImpostos'));
     const results = await resultElement.getText();
-    const expectedResults = [
-        '...CPP: R$ 14000.00',
-        '...ISS: R$ 2500.00',
-        '...PIS: R$ 325.00',
-        '...CSLL: R$ 750.00',
-        '...IRPJ: R$ 4000.00',
-        '...COFINS: R$ 1500.00',
-        '...O total: R$ 22575.00 com base no R$ 20000.00 e a aliquota total 112.88%',
+      const expectedResults = [
+        'CPP: R$ 5600.00',
+        'ISS: R$ 1000.00',
+        'PIS: R$ 130.00',
+        'CSLL: R$ 300.00',
+        'IRPJ: R$ 1600.00',
+        'COFINS: R$ 600.00',
+        'Total de Impostos: R$ 9230.00'
       ];
-      
 
-    console.log(results.split('\n'));
+    // console.log(results.split('\n'));
     console.log(expectedResults);
 
-    if (results.split('\n').toString() === expectedResults.toString()) {
-      console.log('Teste funcional deu certo');
-    } else {
-      console.error('Teste funcional falhou');
-    }
+     if (results.split('\n').toString() === expectedResults.toString()) {
+       console.log('Teste funcional deu certo');
+     } else {
+       console.error('Teste funcional falhou');
+     }
   } catch (error) {
     console.error('Erro durante o teste funcional:', error);
   } finally {
