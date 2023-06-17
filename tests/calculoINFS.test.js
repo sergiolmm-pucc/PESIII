b5aebe0f42b7b82593d8ebc0f4a363ba1716b58b
GRegisPUC <133465373+GRegisPUC@users.noreply.github.com>
2023-06-16 23:59:52 -0300

    Update calculoINFS.test.js

--- tests/calculoINFS.test.js
+++ tests/calculoINFS.test.js
@@ -1,6 +1,6 @@
 const nfServico = require('../pages/Grupo05/calculoINFS');
 
-test('nfServico calculates values correctly for a given input', () => {
+test('nfServico calcula corretamente os valores para uma entrada específica', () => {
   const valorBruto = 180000;
   const expectedDisplay = [
     '...CPP: 4 %',
@@ -13,4 +13,44 @@ test('nfServico calculates values correctly for a given input', () => {
   ];
 
   expect(nfServico(valorBruto)).toEqual(expectedDisplay);
+});
+
+test('nfServico calcula os valores corretamente para uma entrada dentro do segundo intervalo', () => {
+  const valorBruto = 250000;
+  const expectedDisplay = [
+    /...CPP: 4 %/,
+    /...ISS: 2.79 %/,
+    /...PIS: 0 %/,
+    /...CSLL: 0 %/,
+    /...IRPJ: 0 %/,
+    /...COFINS: 1.42 %/,
+    /...O total: 272360 com base no 250000 e a aliquota total 8.21%/,
+  ];
+
+  const receivedDisplay = nfServico(valorBruto);
+
+  // Compare cada linha individualmente usando expressões regulares com tolerância
+  for (let i = 0; i < expectedDisplay.length; i++) {
+    expect(receivedDisplay[i]).toMatch(expectedDisplay[i]);
+  }
+});
+
+test('nfServico calcula corretamente os valores para uma entrada dentro do terceiro intervalo', () => {
+  const valorBruto = 400000;
+  const expectedDisplay = [
+    /...CPP: 4.07 %/,
+    /...ISS: 3\.5[0-9]+ %/,
+    /...PIS: 0.35 %/,
+    /...CSLL: 0.43 %/,
+    /...IRPJ: 0.48 %/,
+    /...COFINS: 1.43 %/,
+    /...O total: 444000 com base no 400000 e a aliquota total 9.91%/,
+  ];
+
+  const receivedDisplay = nfServico(valorBruto);
+
+  // Compare cada linha individualmente usando expressões regulares com tolerância
+  for (let i = 0; i < expectedDisplay.length; i++) {
+    expect(receivedDisplay[i]).toMatch(expectedDisplay[i]);
+  }
 });
